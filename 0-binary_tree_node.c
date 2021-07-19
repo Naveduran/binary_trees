fc442d6d021df18666fa3a33ae01c0c57f0a8af8
Natalia <naveduran@gmail.com>
2021-07-19 16:59:24 -0500

    [Updated] identation

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,17 +9,16 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-  binary_tree_t *new_node;
-  
-  new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node;
 
-  if (new_node == NULL)
-    return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
 
-  new_node->n = value;
-  new_node->parent = parent;
-  new_node->left = NULL;
-  new_node->right = NULL;
-/* Remember to check first if parameters exists */
-  return (new_node);
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
 }
