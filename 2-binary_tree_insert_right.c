4050a6d4916e0f18278b5b6a824c853a4e36f730
Natalia <naveduran@gmail.com>
2021-07-21 15:56:22 -0500

    [Update] check if parent is null

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,6 +12,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *right_node;
 
+	if (parent == NULL)
+		return (NULL);
+
 	right_node = malloc(sizeof(binary_tree_t));
 
 	if (right_node == NULL)
